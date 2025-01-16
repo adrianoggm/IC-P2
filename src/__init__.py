778c288b15290e4046f667594ffca41bd43a4324
Adriano <72881543+adrianoggm@users.noreply.github.com>
2025-01-16 01:33:33 +0100

    prueba

---
+++ src/__init__.py
@@ -0,0 +1,8 @@
+# src/__init__.py
+
+"""
+Paquete principal para el Proyecto QAP con Algoritmos Genéticos.
+
+Contiene módulos para ejecutar variantes del Algoritmo Genético
+y resolver el Problema de Asignación Cuadrática (QAP).
+"""
